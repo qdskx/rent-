7512ed57e43b5429c6f99010c858e16c54619e23
kaixin <qdskx@outlook.com>
2019-02-13 14:38:25 +0800

    添加12.php文件

---
+++ 12.php
@@ -0,0 +1,27 @@
+<?php
+
+$redis = new redis();
+$redis->connect('127.0.0.1' , 6379);
+
+
+
+//$redis->del('one');
+//
+//$redis->zadd('salary' , 90 , 'jack');
+//var_dump($redis->keys('*'));
+//
+//echo $redis->zscore('salary' , 'jack');
+
+
+//$redis->lpush('one' , 'fffffff');
+//$redis->lset('one' , 0 , 'ddddddd');
+//
+//echo $redis->get('one');
+//
+//var_dump($redis->lrange('one' , 0 , 0));
+//
+//var_dump($redis->keys('*'));
+
+echo 'ssssss';
+echo PHP_EOL;
+echo 'dddddddd';
